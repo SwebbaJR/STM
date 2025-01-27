c0d1279ac1d3c048681c7d9e656b9605c6fec690
SwebbaJR <h.ettesvoll@gmail.com>
2025-01-27 18:18:05 +0100

    Implemented PID using Tustin transform

--- resources/controllers/include/controllers/pid_controller.hpp
+++ resources/controllers/include/controllers/pid_controller.hpp
@@ -3,6 +3,7 @@
 
 // TODO: Make low pass filter
 #include <memory>
+#include <cmath>
 #include "filters/first_order_lowpass_iir_filter.hpp"
 
 class PIDController
@@ -13,6 +14,8 @@ public:
         float Kp;
         float Ki;
         float Kd;
+        float max_limit;
+        float min_limit;
         FirstOrderLowpassIIRFilter::FilterDynamicsCoefficient derivative_filter_time_constant;
     };
 
@@ -22,12 +25,13 @@ public:
     float update(const float reference, const float measurement, const float deltaT);
     void set_controller_parameters(ControllerParameters controller_parameters);
 private:
-
+    float anti_windup(const float saturated_output, const float proportional_output, const float derivative_output);
     float saturate_output(const float output);
 
     ControllerParameters controller_parameters;
     float integral_error{0.0};
     float previous_error{0.0};
+    float previous_derivative{0.0};
     std::unique_ptr<FirstOrderLowpassIIRFilter> derivative_filter;
 };
 
--- resources/controllers/src/pid_controller.cpp
+++ resources/controllers/src/pid_controller.cpp
@@ -14,19 +14,43 @@ PIDController::~PIDController()
 float PIDController::update(const float reference, const float measurement, const float deltaT)
 {
     float error = reference - measurement;
-    integral_error += deltaT*error;
-    float filtered_derivative = derivative_filter->update(measurement, deltaT);
 
-    float unsaturated_output = controller_parameters.Kp*error + controller_parameters.Ki*integral_error + controller_parameters.Kd*filtered_derivative;
-    
+    // Integral state using Tustin transform
+    integral_error += deltaT/2.0*(error + previous_error);
+
+    // Derivative termin using Tustin transform
+    float derivative = 2.0/deltaT*(error - previous_error) - previous_derivative;
+    float filtered_derivative = derivative_filter->update(derivative, deltaT);
+
+    // Compute controller compontents
+    float proportional_output = controller_parameters.Kp*error;
+    float integral_output = controller_parameters.Ki*integral_error;
+    float derivative_output = controller_parameters.Kd*filtered_derivative;
+    float unsaturated_output = proportional_output + integral_output + derivative_output;
+
+    // Saturate output
+    float saturated_output = saturate_output(unsaturated_output);
+
+    // Anti-windup: Adjust integral term if output is saturated
+    if (saturated_output != unsaturated_output)
+    {
+        integral_error = anti_windup(saturated_output, proportional_output, derivative_output);
+    }
+
     previous_error = error;
+    previous_derivative = derivative;
 
-    return saturate_output(unsaturated_output);
+    return saturated_output;
 }
 
-float PIDController::saturate_output(const float output)
+float PIDController::saturate_output(const float unsaturated_output)
 {
+    return std::max(controller_parameters.min_limit, std::min(unsaturated_output, controller_parameters.max_limit));
+}
 
+float PIDController::anti_windup(const float saturated_output, const float proportional_output, const float derivative_output)
+{
+    return (saturated_output - proportional_output - derivative_output)/controller_parameters.Ki;
 }
 
 void PIDController::set_controller_parameters(ControllerParameters new_controller_parameters)
